7c2d359988839c53f1c6c14beab9760f20838351
mushopea <mushopea@hotmail.com>
2015-03-27 03:28:53 +0800

    Click (+) then (call button) to see mock UI flow

--- app/src/main/java/sg/edu/nus/taptask/RecordActivity.java
+++ app/src/main/java/sg/edu/nus/taptask/RecordActivity.java
@@ -1,27 +1,24 @@
 package sg.edu.nus.taptask;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class AddCallTaskActivity extends ActionBarActivity {
+public class RecordActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_call_task);
+        setContentView(R.layout.activity_record);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_call_task, menu);
+        getMenuInflater().inflate(R.menu.menu_record, menu);
         return true;
     }
 
@@ -39,15 +36,4 @@ public class AddCallTaskActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-    // go to add new task screen
-    public void onClickContinueButton(View view) {
-        Log.e("Meow", "Recording call activity screen activated");
-
-        Intent intent;
-        intent = new Intent(this, RecordActivity.class);
-        startActivity(intent);
-    }
-
 }
