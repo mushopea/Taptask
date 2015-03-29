9276a103bfae761fece450fa3437f0b922d670da
Low WeiLin <lwl1991@gmail.com>
2015-03-29 15:06:26 +0800

    Add jtransforms library and helper class

---
+++ app/src/main/java/sg/edu/nus/taptask/FFTHelper.java
@@ -0,0 +1,91 @@
+package sg.edu.nus.taptask;
+
+import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
+
+public class FFTHelper {
+
+    public static double[] FFTReal(short[] realInput) {
+        // Convert to complex
+        double[] complex = realToComplex(realInput);
+        // Perform FFT
+        return FFT(complex);
+    }
+
+    public static double[] FFT(double[] complexInput) {
+        // Perform FFT
+        DoubleFFT_1D fft = new DoubleFFT_1D(complexInput.length/2);
+        fft.complexForward(complexInput);
+        return complexInput;
+    }
+
+    public static double[] FFTInverse(double[] complexInput) {
+        // Perform Inverse FFT
+        DoubleFFT_1D fft = new DoubleFFT_1D(complexInput.length/2);
+        fft.complexInverse(complexInput, true);
+        return complexInput;
+    }
+
+    public static double[] complexMultiply(double[] input0, double[] input1) {
+        if (input0.length != input1.length) {
+            return null;
+        }
+        // Multiply FFTs together
+        // complex multiplication: (a + bj) * (c + dj) = (ac - bd) + (bc + ad)j
+        double[] FFTProduct = new double[input0.length];
+        for (int i=0 ; i<512 ; i++) {
+            double a = input0[i*2];
+            double b = input0[i*2+1];
+            double c = input1[i*2];
+            double d = input1[i*2+1];
+
+            FFTProduct[i*2] = a*c - b*d;
+            FFTProduct[i*2+1] = b*c + a*d;
+        }
+        return FFTProduct;
+    }
+
+    public static double[] realToComplex(short[] realInput) {
+        double[] complexOutput = new double[realInput.length*2];
+        for (int i=0 ; i<realInput.length ; i++) {
+            if (i%2 == 0) {
+                complexOutput[i] = realInput[i/2];
+            } else {
+                complexOutput[i] = 0;
+            }
+        }
+        return complexOutput;
+    }
+
+    public static double[] complexRealPart(double[] complexInput) {
+        double[] realOutput = new double[complexInput.length/2];
+        for (int i=0; i<complexInput.length/2; i++) {
+            realOutput[i] = complexInput[2*i];
+        }
+        return realOutput;
+    }
+
+    public static double[] complexMagnitude(double[] complexInput) {
+        double[] magnitude = new double[complexInput.length/2];
+        for (int i=0; i<complexInput.length/2; i++) {
+            double re = complexInput[2*i];
+            double im = complexInput[2*i+1];
+            magnitude[i] = Math.log(re*re + im*im + 0.001);
+        }
+        return magnitude;
+    }
+
+    /**
+     * Gets the number that is a power of 2, and larger or equal to num
+     * Not sure if this works. Might have precision errors...
+     */
+    public static int nextPower2(int num) {
+        double n = Math.log(num)/Math.log(2);
+        double floorN = Math.floor(n);
+        if (n == floorN) {
+            return num;
+        } else {
+            return (int)Math.pow(2, floorN + 1);
+        }
+    }
+
+}
