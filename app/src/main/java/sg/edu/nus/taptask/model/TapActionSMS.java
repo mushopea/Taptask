17e71042061f92327f64c3a0a52e54125be79a0a
Zhang Yiwen <yiwen@pigeonlab.com>
2015-04-12 13:54:25 +0800

    allow silent phone

---
+++ app/src/main/java/sg/edu/nus/taptask/model/TapActionSMS.java
@@ -0,0 +1,27 @@
+package sg.edu.nus.taptask.model;
+
+import android.telephony.SmsManager;
+import android.content.Context;
+
+/**
+ * Created by Yiwen on 12/4/2015.
+ */
+public class TapActionSMS extends TapAction {
+
+    private String smsContent;
+    private String phoneNum;
+
+
+    public TapActionSMS(TapPattern pattern, String smsContent, String phoneNum) {
+        super(pattern);
+        this.smsContent = smsContent;
+        this.phoneNum = phoneNum;
+    }
+
+    public boolean performAction(Context context) {
+//        audio.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+//                AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+        return true;
+    }
+
+}
